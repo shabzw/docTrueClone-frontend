cda42d39de228b01ab4f5e863063b684ed33ede9
codisho <codisho@users.noreply.github.com>
2024-06-20 11:14:19 +0530

    added cursor pointer to icon

--- src/App.tsx
+++ src/App.tsx
@@ -14,33 +14,62 @@ function App() {
       <nav className="bg-white p-4 border-b-4 border-blue-600">
         <div className="container mx-auto flex items-center justify-between">
           {/* Logo */}
-          <img
-            src={logo}
-            className="w-32 md:w-12 lg:w-32 max-w-xs lg:ml-20"
-            alt="Logo"
-          />
+          <a href="#">
+            <img
+              src={logo}
+              className="w-32 md:w-12 lg:w-32 max-w-xs lg:ml-20"
+              alt="Logo"
+            />
+          </a>
 
           {/* Desktop Menu */}
           <div className="hidden md:flex space-x-4">
-            <a href="#" className="text-gray-700 hover:text-blue-600">
+            <a
+              href="/"
+              className="text-gray-700 hover:text-blue-600 active:text-blue-600"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="text-gray-700 hover:text-blue-600 active:text-blue-600"
+            >
               About Us
             </a>
-            <a href="#" className="text-gray-700 hover:text-blue-600">
+            <a
+              href="#"
+              className="text-gray-700 hover:text-blue-600 active:text-blue-600"
+            >
               How it Works
             </a>
-            <a href="#" className="text-gray-700 hover:text-blue-600">
+            <a
+              href="#"
+              className="text-gray-700 hover:text-blue-600 active:text-blue-600"
+            >
               Features
             </a>
-            <a href="#" className="text-gray-700 hover:text-blue-600">
+            <a
+              href="#"
+              className="text-gray-700 hover:text-blue-600 active:text-blue-600"
+            >
               Clinics
             </a>
-            <a href="#" className="text-gray-700 hover:text-blue-600">
+            <a
+              href="#"
+              className="text-gray-700 hover:text-blue-600 active:text-blue-600"
+            >
               Products
             </a>
-            <a href="#" className="text-gray-700 hover:text-blue-600">
+            <a
+              href="#"
+              className="text-gray-700 hover:text-blue-600 active:text-blue-600"
+            >
               Testimonial
             </a>
-            <a href="#" className="text-gray-700 hover:text-blue-600">
+            <a
+              href="#"
+              className="text-gray-700 hover:text-blue-600 active:text-blue-600"
+            >
               Prices
             </a>
           </div>
@@ -139,13 +168,30 @@ function App() {
       <div className="flex flex-col lg:flex-row p-4 lg:p-20 justify-center items-center bg-gray-100 mx-auto">
         {/* Left Content */}
         <div className="w-full lg:w-2/3 p-4 justify-center lg:justify-start">
-          <span className="relative text-lg border-2 border-blue-700 text-blue-600 px-4 py-2 ml-1">
-            Welcome to DocTrue
-            <span className="absolute top-[-8px] left-0 w-2 h-2 bg-blue-700 rounded-full transform translate-x-[-50%] -translate-y-[-50%]"></span>
-            <span className="absolute top-[-8px] right-0 w-2 h-2 bg-blue-700 rounded-full transform translate-x-[50%] -translate-y-[-50%]"></span>
-            <span className="absolute bottom-0 left-0 w-2 h-2 bg-blue-700 rounded-full transform translate-x-[-50%] translate-y-[50%]"></span>
-            <span className="absolute bottom-0 right-0 w-2 h-2 bg-blue-700 rounded-full transform translate-x-[50%] translate-y-[50%]"></span>
-          </span>
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+
+            <span className="relative text-lg border-2 border-blue-700 text-blue-600 px-4 py-2 ml-1">
+              Welcome to DocTrue
+              <span className="absolute top-[-8px] left-0 w-2 h-2 bg-blue-700 rounded-full transform translate-x-[-50%] -translate-y-[-50%]"></span>
+              <span className="absolute top-[-8px] right-0 w-2 h-2 bg-blue-700 rounded-full transform translate-x-[50%] -translate-y-[-50%]"></span>
+              <span className="absolute bottom-0 left-0 w-2 h-2 bg-blue-700 rounded-full transform translate-x-[-50%] translate-y-[50%]"></span>
+              <span className="absolute bottom-0 right-0 w-2 h-2 bg-blue-700 rounded-full transform translate-x-[50%] translate-y-[50%]"></span>
+            </span>
+          </div>
 
           <div className="text-3xl lg:text-6xl font-bold mt-4 lg:mt-6">
             <div className="flex items-center">
